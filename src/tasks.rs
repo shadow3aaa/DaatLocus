7abd14c664b801f71de3d927fd3e4331472c20b9
shadow3aaa <74781933+shadow3aaa@users.noreply.github.com>
2026-03-12 21:25:06 +0800

    Add current task selection

--- src/tasks.rs
+++ src/tasks.rs
@@ -8,6 +8,7 @@ use crate::get_spinova_home;
 
 #[derive(Serialize, Deserialize, Default, Clone)]
 pub struct Tasks {
+    working_task: Option<usize>,
     tasks: Vec<Task>,
 }
 
@@ -24,6 +25,15 @@ impl Tasks {
             .ok()
             .and_then(|data| postcard::from_bytes::<Self>(&data).ok())
             .unwrap_or_else(|| Self::default())
+    }
+
+    pub fn select_working_task(&mut self, index: usize) -> Option<&Task> {
+        if index > 0 && index <= self.tasks.len() {
+            self.working_task = Some(index - 1);
+            self.tasks.get(index - 1)
+        } else {
+            None
+        }
     }
 
     pub fn add_task(&mut self, description: String) {
@@ -49,8 +59,25 @@ impl Tasks {
 
 impl Display for Tasks {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if self.tasks.is_empty() {
+            return Ok(());
+        }
+
+        if self.working_task.is_none() {
+            writeln!(
+                f,
+                "当前没有选中的任务。如果要执行任务，必须先选择一个任务再执行。"
+            )?;
+        }
+
         for (i, task) in self.tasks.iter().enumerate() {
-            writeln!(f, "{}. {}", i + 1, task.description)?;
+            if Some(i) == self.working_task {
+                writeln!(f, "--- 选中的任务 ---")?;
+                writeln!(f, "{}. {}", i + 1, task.description)?;
+                writeln!(f, "--- 选中的任务 ---")?;
+            } else {
+                writeln!(f, "{}. {}", i + 1, task.description)?;
+            }
         }
         Ok(())
     }
