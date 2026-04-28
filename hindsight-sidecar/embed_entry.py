953b3ff7d18b8984ec4519c57f7be35f3bd66e21
shadow3 <shadow3aaaa@gmail.com>
2026-04-28 18:09:48 +0800

    Patch Windows Hindsight metadata replacement

--- hindsight-sidecar/embed_entry.py
+++ hindsight-sidecar/embed_entry.py
@@ -1,3 +1,4 @@
+import json
 import os
 import sys
 from pathlib import Path
@@ -30,8 +31,30 @@ if sys.platform == "win32":
     def _unlock_file(file_obj):
         _with_lock_cursor_at_start(file_obj, msvcrt.LK_UNLCK)
 
+    def _save_metadata(self, metadata):
+        self._ensure_directories()
+
+        metadata_file = self._get_metadata_file()
+        temp_file = metadata_file.with_suffix(".json.tmp")
+
+        with open(temp_file, "w", encoding="utf-8") as file_obj:
+            profile_manager.lock_file(file_obj)
+            try:
+                json.dump(
+                    {"version": metadata.version, "profiles": metadata.profiles},
+                    file_obj,
+                    indent=2,
+                )
+                file_obj.flush()
+                os.fsync(file_obj.fileno())
+            finally:
+                profile_manager.unlock_file(file_obj)
+
+        temp_file.replace(metadata_file)
+
     profile_manager.lock_file = _lock_file
     profile_manager.unlock_file = _unlock_file
+    profile_manager.ProfileManager._save_metadata = _save_metadata
 
 
 def _find_bundled_api_command(self):
